8d8f7d199f2a23bf06da8423627f3d084e536c38
Beri28 <cletusberinyuy@gmail.com>
2025-01-16 15:31:12 +0100

    local chnages

--- Auth.py
+++ Auth.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import pytest
 
 def test_email_input_presence_register(driver):
     try:
@@ -64,13 +65,19 @@ def test_successful_registration_as_customer(driver):
         # Find and fill the signup form
         driver.get("http://localhost:5000/register")
         driver.find_element(By.NAME, "name").send_keys("john")
+        time.sleep(2)
         email_input = driver.find_element(By.NAME, "email")
         email_input.send_keys("john@gmail.com")
+        time.sleep(2)
         driver.find_element(By.NAME, "address").send_keys("molyko")
+        time.sleep(2)
         password_input = driver.find_element(By.NAME, "password")
         password_input.send_keys("12345678")
+        time.sleep(2)
         driver.find_element(By.NAME, "contact").send_keys("680124356")
-        driver.find_element(By.NAME, "account_Type").send_keys("customer")
+        time.sleep(2)
+        driver.find_element(By.NAME, "account_Type").send_keys("restaurant-manager")
+        time.sleep(2)
         submit_button = driver.find_element(By.ID, "register")
         submit_button.click()
         time.sleep(5)
@@ -108,6 +115,34 @@ def test_login_button_presence(driver):
     except Exception as e:
         print(e)
 
+def test_successful_add_to_todays_menu(driver):
+    try:
+        driver.get("http://localhost:5000/login")
+        email_input = driver.find_element(By.NAME, "email")
+        password_input = driver.find_element(By.NAME, "password")
+        submit_button = driver.find_element(By.ID, "login")
+        email_input.send_keys("bongyu@gmail.com")
+        password_input.send_keys("12345678")
+        submit_button.click()
+        time.sleep(3)
+        add_to_todays_menu = driver.find_elements(By.CLASS_NAME, "todays-menu")
+        r_add_to_todays_menu = driver.find_elements(By.CLASS_NAME, "r-todays-menu")
+        print(add_to_todays_menu[1])
+        print("////////////////////")
+        for i,element in enumerate(add_to_todays_menu):
+            if "d-none" in element.get_attribute("class"):
+                r_add_to_todays_menu[i].click()
+                time.sleep(2)
+                # assert "d-none" in r_add_to_todays_menu[i].get_attribute("class")
+            elif "d-none" not in element.get_attribute("class") :
+                element.click()
+                time.sleep(2)
+                # assert "d-none" in element.get_attribute("class")
+            else:
+                raise AssertionError("Test failed. Couldn't find button")
+        time.sleep(2)
+    except Exception as e:
+        print(e)
 def test_successful_login(driver):
     try:
         driver.get("http://localhost:5000/login")
